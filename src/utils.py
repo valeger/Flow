1f9c5a37c2f3f5b0cbc2a0671f7ddfae2ba7ed9a
valeger <gerasimenkovaleriya.i@gmail.com>
2020-07-19 18:16:45 +0300

    add train and eval functions

--- src/utils.py
+++ src/utils.py
@@ -54,3 +54,59 @@ def interpolate(model: nn.Module, test_loader: DataLoader) -> np.ndarray:
         results = preprocess(logit_results, reverse=True)
         return results.cpu().numpy()
 
+def train(model: nn.Module, 
+          train_loader: DataLoader, 
+          optimizer: Optimizer,
+          device: torch.device) -> torch.Tensor:
+    losses = []
+    for images in train_loader:
+        images = images.to(device)
+        logit_x, log_det = preprocess(images.float())
+        log_prob = model.log_prob(logit_x)
+        log_prob += log_det
+
+        batch_loss = -torch.mean(log_prob) / (3.0 * 32.0 * 32.0)
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+        batch_loss = float(batch_loss.data)
+        losses.append(batch_loss)
+    return losses
+
+def train_epochs(model: nn.Module,
+                 train_loader: DataLoader,
+                 test_loader: DataLoader,
+                 params: Dict) -> Tuple[List]:
+    epochs, lr, fname = params['epochs'], params['lr'], params['name']
+    device = params['device']
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    
+    train_losses = []
+    test_losses = []
+
+    for epoch in range(epochs):
+        losses_per_epoch = train(model, train_loader, optimizer, device)
+        test_loss = eval_loss(model, test_loader, device)
+        train_losses.extend(losses_per_epoch)
+        test_losses.append(test_loss)
+        
+        print(f'Epoch {epoch} Train loss: {np.mean(losses_per_epoch):.4f}\n')
+        print(f'Epoch {epoch} Test loss: {test_loss:.4f}\n')
+
+    model.save_model(fname)
+    return train_losses, test_losses
+
+def eval_loss(model: nn.Module, test_loader: DataLoader, device: torch.device) -> np.float64:
+    model.eval()
+    losses = []
+    for images in test_loader:
+        with torch.no_grad():
+            images = images.to(device)
+            logit_x, log_det = preprocess(images.float())
+            log_prob = model.log_prob(logit_x)
+            log_prob += log_det
+
+            loss = -torch.mean(log_prob) / (3.0 * 32.0 * 32.0)
+            loss = loss.item()
+            losses.append(loss)
+    return np.mean(losses)
